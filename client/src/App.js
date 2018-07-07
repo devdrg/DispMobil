00c80c29b4b3f11383c003a2e755d8fca3a6e6e2
Dragos <dragos@dragos.website>
2018-07-07 18:52:51 +0300

    set up heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  Container } from 'reactstrap';
+
+
+import NavBar from './components/NavBar';
+import MainBody from './components/MainBody';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>saloot</h1>
+      <div className="Container">
+        <Container>
+          <NavBar />
+          <MainBody />
+          </Container>
       </div>
     );
   }
---
+++ client/src/components/MainBody.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MainBody extends Component {
+    render(){
+        return(
+            <div>
+                <h3>Content</h3>
+            </div>
+        );
+    }
+};
+
+export default MainBody;
